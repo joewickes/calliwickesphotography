ba191c01a3c432f0934f38b20b30c5ecc14e5cf6
Joe Wickes <joe.c.wickes@gmail.com>
2024-01-25 23:22:27 -0500

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,66 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.calliwickesphotography.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.calliwickesphotography.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.calliwickesphotography.com/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.calliwickesphotography.com/experience',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.calliwickesphotography.com/familygallery',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://www.calliwickesphotography.com/portfolio',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://www.calliwickesphotography.com/pricing',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.4,
+    },
+    {
+      url: 'https://www.calliwickesphotography.com/seniorgallery',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    // {
+    //   url: 'https://www.calliwickesphotography.com/privacypolicy',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'yearly',
+    //   priority: 0.2,
+    // },
+    // {
+    //   url: 'https://www.calliwickesphotography.com/termsandconditions',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'yearly',
+    //   priority: 0.2,
+    // },
+  ];
+}
