92ca3234ec0ea20558ccf7d2256fa9f25daa1814
Joe Wickes <joe.c.wickes@gmail.com>
2024-01-26 17:35:48 -0500

    Defer fullstory load

--- src/app/FullstoryInit.tsx
+++ src/app/FullstoryInit.tsx
@@ -2,7 +2,7 @@ import Script from 'next/script';
 
 const FullStoryInit = () => {
   return (
-    <Script id="fs-init">
+    <Script id="fs-init" strategy="lazyOnload">
       {`
         window['_fs_host'] = 'fullstory.com';
         window['_fs_script'] = 'edge.fullstory.com/s/fs.js';
