af6f76c112041bfe2359e0b274454e5103fc335a
Joe Wickes <joe.c.wickes@gmail.com>
2024-01-25 19:51:58 -0500

    Update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,6 +11,27 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#fff",
     icons: [
       {
+        src: "@/../public/assets/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "@/../public/assets/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "@/../public/assets/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "@/../public/assets/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
         src: "/favicon.ico",
         sizes: "any",
         type: "image/x-icon",
