ef7a23185af4e8043b9350021048022b8f3316dc
Joe Wickes <joe.c.wickes@gmail.com>
2024-03-15 16:27:30 -0400

    Add other remote pattern

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: 'cwp-strapi-62335048859d.herokuapp.com' }],
+    remotePatterns: [{ hostname: 'cwp-strapi-62335048859d.herokuapp.com' }, { hostname: 'res.cloudinary.com' }],
   },
 };
 
