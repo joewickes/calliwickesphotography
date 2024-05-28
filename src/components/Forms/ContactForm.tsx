8292e18104874345fdc3e43dd1140df5c7b60e1b
Joe Wickes <joe.c.wickes@gmail.com>
2024-05-28 08:18:13 -0400

    Change api call string

--- src/components/Forms/ContactForm.tsx
+++ src/components/Forms/ContactForm.tsx
@@ -53,7 +53,7 @@ const ContactForm = ({
       message,
     };
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/email`, {
+    const res = await fetch(`/api/email`, {
       method: 'POST',
       body: JSON.stringify(data),
     });
