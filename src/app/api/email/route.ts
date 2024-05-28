56c1e57d11de9abf01b0ab1ecc4920ead8719d00
Joe Wickes <joe.c.wickes@gmail.com>
2024-05-28 09:08:04 -0400

    Fix newletter signup

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
     console.log('is newsletter?', data.newsletter);
     if (data.newsletter) {
       email = `Name: ${data.name}\nEmail: ${data.email}`;
-      await signUpForNewsletter(data.email);
+      await signUpForNewsletter(email);
       return Response.json({ message: 'Email sent successfully' }, { status: 200 });
     } else {
       email = `Name: ${data.name}\nEmail: ${data.email}\nPhone: ${data.phone}\nMessage: ${data.message}`;
