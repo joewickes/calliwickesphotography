55731dec758d0342766c794ab3ef5814c841a260
Joe Wickes <joe.c.wickes@gmail.com>
2024-07-12 19:45:20 -0400

    More styling changeS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -454,7 +454,7 @@ export default async function Home() {
             <span className="font-thin leading-8 text-right ">
               <BlocksRenderer content={data.preExperienceParagraph} />
             </span>
-            <div className="pb-[0px] pt-[50px] mt-[60px] sm:flex sm:justify-end">
+            <div className="pb-[0px] pt-[50px] sm:flex sm:justify-end">
               <Link href={data.preExperienceButtonLink || '#'} legacyBehavior passHref>
                 <a className="mb-[100px] border border-black py-[15px] text-[13px] tracking-[.35em] px-[30px] flex sm:inline justify-center text-center">
                   {data.preExperienceButtonText || 'test'}
