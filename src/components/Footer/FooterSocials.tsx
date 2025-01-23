fd4baf583aef465ecf6f2f1adb47c0a4f5cd61c3
Joe Wickes <joe.c.wickes@gmail.com>
2025-01-23 13:45:35 -0500

    Add descriptions to social svgs

--- src/components/Footer/FooterSocials.tsx
+++ src/components/Footer/FooterSocials.tsx
@@ -34,30 +34,62 @@ const FooterSocials = ({
   return (
     <div className="flex flex-wrap justify-start">
       <a target="_blank" aria-label="Facebook" href={facebookLink}>
-        <FacebookLogo size={23} className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]" />
+        <FacebookLogo
+          size={23}
+          className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]"
+          aria-description="Facebook"
+        />
       </a>
       <a target="_blank" aria-label="Instagram" href={instagramLink}>
-        <InstagramLogo size={23} className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]" />
+        <InstagramLogo
+          size={23}
+          className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]"
+          aria-description="Instagram"
+        />
       </a>
       <a target="_blank" aria-label="Pinterest" href={pinterestLink}>
-        <PinterestLogo size={23} className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]" />
+        <PinterestLogo
+          size={23}
+          className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]"
+          aria-description="Pinterest"
+        />
       </a>
       <a target="_blank" aria-label="Yelp" href={yelpLink}>
-        <FontAwesomeIcon icon={faYelp} className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]" />
+        <FontAwesomeIcon
+          icon={faYelp}
+          className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]"
+          aria-description="Yelp"
+        />
       </a>
       <a target="_blank" aria-label="LinkedIn" href={linkedInLink}>
-        <LinkedinLogo size={23} className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]" />
+        <LinkedinLogo
+          size={23}
+          className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]"
+          aria-description="LinkedIn"
+        />
       </a>
       <a target="_blank" aria-label="YouTube" href={youtubeLink}>
-        <YoutubeLogo size={23} className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]" />
+        <YoutubeLogo
+          size={23}
+          className="mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]"
+          aria-description="YouTube"
+        />
       </a>
 
       <a target="_blank" aria-label="Spotify" href={spotifyLink}>
-        <SpotifyLogo size={23} className=" mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]" />
+        <SpotifyLogo
+          size={23}
+          className=" mr-[20px] cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]"
+          aria-description="Spotify"
+        />
       </a>
 
       <a target="_blank" aria-label="Apple Podcasts" href={applePodcastsLink}>
-        <ApplePodcastsLogo size={23} className="cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]" />
+        <ApplePodcastsLogo
+          size={23}
+          className="cursor-pointer mt-[20px] min-h-[24px] min-w-[24px]"
+          aria-description="Apple Podcasts"
+        />
       </a>
     </div>
   );
